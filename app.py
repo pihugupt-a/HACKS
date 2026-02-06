fe145e510afcc5cb8de23f36ee918b5705ce0116
pihugupt-a <gpihu@tisb.ac.in>
2026-02-06 19:22:11 +0530

    Fix indentation for divider in app.py

--- app.py
+++ app.py
@@ -734,7 +734,7 @@ elif st.session_state.page == "GreenScore":
             st.error("❌ Product not found in database.")
         else:
             r = result.iloc[0]
-                st.divider()
+            st.divider()
         if "selected_alternative" in st.session_state:
             del st.session_state["selected_alternative"]
         # Clear selected alternative AFTER it has been applied
